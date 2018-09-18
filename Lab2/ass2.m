4ebbfc5e7db1f5f904feb89baa8d851ef32212c8
C.T.F. Grier Mulvenna <s2886790@156-32.ub.rug.nl>
2018-09-18 15:30:22 +0200

    Assignment 2 started

---
+++ Lab2/ass2.m
@@ -0,0 +1,12 @@
+load('lab1_2.mat');
+data = measurements;
+
+body_height = data(:,1);
+hair_length = data(:,2);
+
+x = [0 59];
+y = [150 200];
+
+scatter(hair_length, body_height);
+hold on
+line(x,y);
