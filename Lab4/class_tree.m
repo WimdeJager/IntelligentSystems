3d187118ae5bd67bae92ce4727337843b663e2d6
C.T.F. Grier Mulvenna <s2886790@156-39.ub.rug.nl>
2018-10-13 17:09:51 +0200

    Some cleaning; Assignment 5 init

---
+++ Lab4/class_tree.m
@@ -0,0 +1,26 @@
+%Killer whale
+%length = 6-8m
+%visible when dives
+%tall and pointed dorsal fin
+%blows water often
+
+%Beluga whale
+%Not visible when diving
+%No dorsal fin
+
+%Narwhal whale
+%up to 5m length
+%Long tusk
+%fluke visible when diving
+%No dorsal fin
+
+%Bowhead whale
+%Fluke visible when diving
+%No dorsal fin
+%Up to 20m
+
+%Blue whale
+%over 30m long
+%fluke visible when diving
+%Dorsal fin visible but small
+tree = fitctree(Tbl,ResponseVarName)
