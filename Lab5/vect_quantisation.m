b50245d7e703ecca5a29c48c9dca8794fad66db1
C.T.F. Grier Mulvenna <s2886790@156-37.ub.rug.nl>
2018-10-14 20:47:19 +0200

    Quantisation error

--- Lab5/vect_quantisation.m
+++ Lab5/vect_quantisation.m
@@ -25,10 +25,18 @@ end
 
 % Begin epochs
 % distances has extra column for idxs used in computation
-distances = zeros(example_count - num_prototypes, num_prototypes);
+data = x;
+data(prototypes(:,3), :) = [];
+f = figure('visible','on');
+scatter(data(:,1), data(:,2), 'MarkerEdgeColor',[0 .5 .5],...
+              'MarkerFaceColor',[0 .7 .7],...
+              'LineWidth',1.5);
+hold on
+
+distances = zeros(example_count, num_prototypes);
 distance_count = 0;
-epoch_max = 1;
-step_size = 0.6
+epoch_max = 10;
+step_size = 0.1;
 for i = 1:epoch_max
 	rand_set1_idxs = randperm(example_count);
 	for j = 1:example_count
@@ -50,5 +58,30 @@ for i = 1:epoch_max
 		new_prototype(step_size, winner_x, winner_y, example_x, example_y);
     end
   end
+
+	% Calculate the quantisation error
+	q_distances = [0 0];
+	sum = 0;
+	for i = 1:example_count
+		for k = 1:num_prototypes
+			example_x = set1(i,1);
+			example_y = set1(i,2);
+			prototype_x = prototypes(k,1);
+			prototype_y = prototypes(k,2);
+			x_diff = abs(prototype_x - example_x);
+			y_diff = abs(prototype_y - example_y);
+			euclidian_dist = sqrt(x_diff^2 + y_diff^2);
+			q_distances(k) = euclidian_dist;
+		end
+		sum = sum + min(q_distances);
+	end
+
+	% Plot epoch
+	new_pos = scatter(prototypes(:,1), prototypes(:,2), 'MarkerEdgeColor',[0 .5 .5],...
+              'MarkerFaceColor',[1 0 0],...
+              'LineWidth',1.5);
+	filename = sprintf('%s_%d','epochs',i)
+	saveas(f, filename, 'png');
+	delete(new_pos);
 end
 
