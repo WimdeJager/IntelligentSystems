6f18d54169675497d078982948df4d2aa223e55a
wimdj27 <wimdejager72@gmail.com>
2018-10-14 17:21:08 +0200

    Add files via upload

--- Lab5/vect_quantisation.m
+++ Lab5/vect_quantisation.m
@@ -6,16 +6,16 @@ load('w6_1z.mat');
 set1 = w6_1x;
 set2 = w6_1y;
 set3 = w6_1z;
-example_count = 1000;
+[example_count, dimension]  = size(set1);
 
 % Getting prototypes
-num_prototypes = 2 % Number of prototypes
+num_prototypes = 2; % Number of prototypes
 prototypes = zeros(num_prototypes,3); % 3rd columns records idx in dataset
 for i = 1:num_prototypes
 	r = randi(example_count);
 	if i ~= 1
 		while ismember(r,prototypes(:,3)) ~= 0
-			r = randi(example_count)
+			r = randi(example_count);
 		end
 	end
 	prototypes(i,1) = set1(r,1);
@@ -25,7 +25,7 @@ end
 
 % Begin epochs
 % distances has extra column for idxs used in computation
-distances = zeros(example_count - num_prototypes, num_prototypes + 1);
+distances = zeros(example_count - num_prototypes, num_prototypes);
 distance_count = 0;
 epoch_max = 1;
 for i = 1:epoch_max
@@ -34,16 +34,22 @@ for i = 1:epoch_max
 		if ismember(j,prototypes(:,3)) == 0 % example is not prototype
 			example_x = set1(rand_set1_idxs(j),1);
 			example_y = set1(rand_set1_idxs(j),2);
-			distance_count = distance_count + 1;
-			distances(distance_count, 3) = rand_set1_idxs(j);
 			for k = 1:num_prototypes
 				prototype_x = prototypes(k,1);
 				prototype_y = prototypes(k,2);
-				x_diff = prototype_x-example_x;
-				y_diff = prototype_y-example_y;
+				x_diff = prototype_x - example_x;
+				y_diff = prototype_y - example_y;
 				euclidian_dist = sqrt(x_diff^2 + y_diff^2);
-				distances(distance_count, k) = euclidian_dist;
-			end
-		end
-	end
+				distances(j, k) = euclidian_dist;
+      end
+      [winner_dist, winner_idx] = min(distances(j,:));
+      winner_x = prototypes(winner_idx, 1);
+      winner_y = prototypes(winner_idx, 2);
+      
+      prototypes(winner_idx, 1) = (winner_x - example_x)/2;
+      prototypes(winner_idx, 2) = (winner_y - example_y)/2;
+    end
+  end
 end
+
+
