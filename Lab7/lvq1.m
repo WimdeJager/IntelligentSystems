c28c737c74a821ebd04b41ef84f7e37b36d770f4
C.T.F. Grier Mulvenna <s2886790@156-44.ub.rug.nl>
2018-10-27 12:48:10 +0200

    LVQ-1 Training Function

--- Lab7/lvq1.m
+++ Lab7/lvq1.m
@@ -1,126 +1,42 @@
-<<<<<<< HEAD
-load('data_lvq.mat');
-
-% Defining some names
-data = w5_1;
-[example_count, dimension]  = size(data);
-class1 = data(1:example_count/2,:);
-class2 = data((example_count/2)+1 : example_count, :);
-
-% Defining prototypes (x,y,i,j) where x and y are the features from our input
-% vectors. i is the index from the input and j is class 1 or 2.
-prototype_count = 2;
-prototypes = zeros(prototype_count,4);
-
-for i = 1:prototype_count
-    if i <= prototype_count/2
-        r = randi(example_count/2);
-    else
-        r = randi(example_count/2) + 50;
-    end
-    if i ~= 1
-        while ismember(r,prototypes(:,3)) ~= 0
-            if i <= prototype_count/2
-                r = randi(example_count/2);
-            else
-                r = randi(example_count/2) + 50;
-            end
-        end
-    end
-    prototypes(i,1) = data(r,1);
-    prototypes(i,2) = data(r,2);
-    prototypes(i,3) = r;
-    if r <= 50
-        prototypes(i,4) = 1 ;
-    else
-        prototypes(i,4) = 2 ;
-    end
-end
-
-% Remove prototypes from class variables so we don't plot the prototypes
-% 2 times
-class1(prototypes(1:prototype_count/2,3),:) = [];
-class2(prototypes((prototype_count/2)+1:prototype_count,3)-50,:) = [];
-
-
-% Plot examples
-close all;
-f = figure('visible','on', 'pos',[0 0 1920 1080]); % Plotting examples
-scatter(class1(:,1), class1(:,2),'MarkerEdgeColor',[0 0 0], ...
-    'MarkerFaceColor',[0 0 0], 'LineWidth', 1.5);
-hold on
-scatter(class2(:,1), class2(:,2), 'MarkerEdgeColor',[0 0 0], ...
-    'MarkerFaceColor',[1 1 1], 'LineWidth',1.5);
-
-% Begin epochs
-distances_to_prototypes = zeros(example_count, prototype_count);
-epoch_max = 100;
-step_size = 0.002;
-training_error = zeros(1,epoch_max);
-for i = 1:epoch_max
-    rand_example_idxs = randperm(example_count);
-    training_error_sum = 0;
-    
-    for j = 1:example_count
-        if ismember(j,prototypes(:,3)) == 0 % example is not prototype
-            example_x = data(rand_example_idxs(j),1);
-            example_y = data(rand_example_idxs(j),2);
-            for k = 1:prototype_count
-                prototype_x = prototypes(k,1);
-                prototype_y = prototypes(k,2);
-                x_diff = abs(prototype_x - example_x);
-                y_diff = abs(prototype_y - example_y);
-                euclidian_dist = sqrt(x_diff^2 + y_diff^2);
-                distances_to_prototypes(j, k) = euclidian_dist;
-            end
-            
-            [winner_dist, winner_idx] = min(distances_to_prototypes(j,:));
-            winner_x = prototypes(winner_idx, 1);
-            winner_y = prototypes(winner_idx, 2);
-            if ((prototypes(winner_idx,4) == 1) && (j <= 50)) ...
-                    || ((prototypes(winner_idx,4) == 2) && (j > 50)) % same class
-                prototypes(winner_idx,1:2) = ...
-                    new_prototype(step_size, winner_x, winner_y, example_x, example_y);
-            else % different class so reflect example point over protoype then move
-                training_error_sum = training_error_sum + 1;
-                if example_x > winner_x
-                    example_x = winner_x - x_diff;
-                else
-                    example_x = winner_x + x_diff;
-                end
 function prototypes = lvq1(training_set, prototype_count, step_size, epoch_max)
 % Defining some names
-[example_count, dimension]  = size(training_set);
-class1 = training_set(1:example_count/2,:);
-class2 = training_set((example_count/2)+1 : example_count, :);
+[example_count, training_set_dimension] = size(training_set);
+class1_bools = training_set(:,3) == 1;
+class2_bools = training_set(:,3) == 2;
+class1 = training_set(class1_bools, :);
+class2 = training_set(class2_bools);
+[class1_size, class1_dimension] = size(class1);
+[class2_size, class2_dimension] = size(class2);
 
 % Defining prototypes (x,y,i,j) where x and y are the features from our input
 % vectors. i is the index from the input and j is class 1 or 2.
 prototypes = zeros(prototype_count,4);
-for i = 1:prototype_codataunt
+for i = 1:prototype_count
     if i <= prototype_count/2
-        r = randi(example_count/2);
+        r = randi(class1_size);
     else
-        r = randi(example_count/2) + 50;
+        r = randi(class2_size) + class1_size;
     end
     if i ~= 1
         while ismember(r,prototypes(:,3)) ~= 0
-            if i <= prototype_count/2
-                r = randi(example_count/2);
+            if i <= class1_size
+                r = randi(class1_size);
             else
-                r = randi(example_count/2) + 50;
+                r = randi(class2_size) + class1_size;
             end
         end
     end
     prototypes(i,1) = training_set(r,1);
     prototypes(i,2) = training_set(r,2);
     prototypes(i,3) = r;
-    if r <= 50
+    
+    if i <= prototype_count/2
         prototypes(i,4) = 1 ;
     else
         prototypes(i,4) = 2 ;
     end
 end
+
 
 % Remove prototypes from class variables so we don't plot the prototypes
 % 2 times
